74c2230d8166a6106de74322b95120e30759d65f
unknown <hruales1138@gmail.com>
2022-09-23 13:43:53 -0500

    Homework 4

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -3,8 +3,57 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor) {
+    if(typeof executor !== 'function') throw TypeError('Executor must be a function');
+    this._state = 'pending';
+    this._value = undefined;
+    this._handlerGroups = [];
+    executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+};
 
+$Promise.prototype._internalResolve = function(data){
+    if(this._state === 'pending') {
+        this._state = 'fulfilled';
+        this._value = data;
+        this._callHandlers();
+    }
+};
 
+$Promise.prototype._internalReject = function(data){
+    if(this._state === 'pending') {
+        this._state = 'rejected';
+        this._value = data;
+        this._callHandlers();
+    }
+};
+
+$Promise.prototype.then = function(successCb, errorCb) {
+    if(typeof successCb !== 'function') successCb = false;
+    if(typeof errorCb !== 'function') errorCb = false;
+    this._handlerGroups.push({successCb, errorCb});
+    if(this._state !== 'pending') this._callHandlers();
+};
+
+$Promise.prototype._callHandlers = function() {
+    while(this._handlerGroups.length > 0) {
+        let current = this._handlerGroups.shift();
+
+        if(this._state === 'fulfilled') {
+            if(current.successCb) {
+                current.successCb(this._value);
+            };
+        }
+        if(this._state === 'rejected') {
+            if(current.errorCb) {
+                current.errorCb(this._value);
+            };
+        }
+    }
+};
+   
+$Promise.prototype.catch = function(errorCb) {
+    return this.then(null, errorCb)
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
